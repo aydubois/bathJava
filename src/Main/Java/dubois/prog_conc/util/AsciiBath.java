68b1d4d0f1650f1191297bc1b8a6fac2563ccf4e
Audrey <ay.dubois@gmail.com>
2021-03-02 08:01:57 +0100

    [ADD] explications clearConsole

--- src/Main/Java/dubois/prog_conc/util/AsciiBath.java
+++ src/Main/Java/dubois/prog_conc/util/AsciiBath.java
@@ -16,17 +16,18 @@ public class AsciiBath {
                             "     _/ (_   / _/  " +ConsoleColors.YELLOW+
             "___( ^)"+ConsoleColors.PURPLE+">\n"+ConsoleColors.RESET;
         ;
-    private String full0Bath =  ",.--------------------------,,\n"+
+    private String full0Bath =  "\n"+"\n"+"\n"+"\n"+"\n"+
+            ",.--------------------------,,\n"+
                                 "|%`````````````````````````` |\n"+
                                 "|%.                          |\n"+
                                 "|%%                          |\n"+
                                 " +%%.                      .+'\n";
-    private String full1Bath =  ",.--------------------------,,\n"+
+    private String full1Bath =  "\n"+"\n"+"\n"+"\n"+"\n"+",.--------------------------,,\n"+
                                 "|%`````````````````````````` |\n"+
                                 "|%.                          |\n"+
                                 "|%%                          |\n"+
                                 " +%%."+ConsoleColors.BLUE_BACKGROUND+"                      "+ConsoleColors.RESET+".+'\n";
-    private String full2Bath =  ",.--------------------------,,\n"+
+    private String full2Bath =  "\n"+"\n"+"\n"+"\n"+"\n"+",.--------------------------,,\n"+
                                 "|%`````````````````````````` |\n"+
                                 "|%.                          |\n"+
                                 "|%%"+ConsoleColors.BLUE_BACKGROUND+"                          "+ConsoleColors.RESET+"|\n"+
@@ -40,18 +41,29 @@ public class AsciiBath {
                                 " +%%."+ConsoleColors.BLUE_BACKGROUND+"                      "+ConsoleColors.RESET+".+'\n";
     private String leak0Bath =  " ``--f t--------------f t'°\n"+
                                 "    {___}            {___}\n";
-    private String leak1BathEmpty =  " ``--f t-------- -----f t'°         REPARATION EN COURS\n"+
-                                "    {___}            {___}\n";
+    private String leak1BathEmpty = " ``--f t-------- -----f t'°         REPARATION EN COURS\n"+
+                                    "    {___}            {___}\n"+
+                                    "              "+ConsoleColors.BLUE+"~~~~"+ConsoleColors.RESET+"        ";
     private String leak2BathEmpty =  " ``--f t-------    ---f t'°         REPARATION EN COURS\n"+
-                                "    {___}            {___}\n";
+                                "    {___}            {___}\n" +
+                                "              "+ConsoleColors.BLUE+"~~~~~~"+ConsoleColors.RESET+"        ";
+
     private String leak3BathEmpty =  " ``--f t-----      ---f t'°         REPARATION EN COURS\n"+
-                                "    {___}            {___}\n";
+                                "    {___}            {___}\n" +
+            "            "+ConsoleColors.BLUE+"~~~~~~~~"+ConsoleColors.RESET+"       ";
+
     private String leak1Bath =  " ``--f t--------"+ConsoleColors.BLUE_BACKGROUND+" "+ConsoleColors.RESET+"-----f t'°\n"+
-                                "    {___}       "+ConsoleColors.BLUE_BACKGROUND+" "+ConsoleColors.RESET+"    {___}\n";
+                                "    {___}       "+ConsoleColors.BLUE_BACKGROUND+" "+ConsoleColors.RESET+"    {___}\n"+
+            "              "+ConsoleColors.BLUE_BACKGROUND+"    "+ConsoleColors.RESET+"        ";
+
     private String leak2Bath =  " ``--f t-------"+ConsoleColors.BLUE_BACKGROUND+"    "+ConsoleColors.RESET+"---f t'°\n"+
-                                "    {___}      "+ConsoleColors.BLUE_BACKGROUND+"    "+ConsoleColors.RESET+"  {___}\n";
+                                "    {___}      "+ConsoleColors.BLUE_BACKGROUND+"    "+ConsoleColors.RESET+"  {___}\n"+
+            "              "+ConsoleColors.BLUE_BACKGROUND+"      "+ConsoleColors.RESET+"        ";
+
     private String leak3Bath =  " ``--f t-----"+ConsoleColors.BLUE_BACKGROUND+"      "+ConsoleColors.RESET+"---f t'°\n"+
-                                "    {___}    "+ConsoleColors.BLUE_BACKGROUND+"      "+ConsoleColors.RESET+"  {___}\n";
+                                "    {___}    "+ConsoleColors.BLUE_BACKGROUND+"      "+ConsoleColors.RESET+"  {___}\n"+
+            "            "+ConsoleColors.BLUE_BACKGROUND+"        "+ConsoleColors.RESET+"       ";
+
 
 
 
@@ -129,7 +141,8 @@ public class AsciiBath {
     public void animateGif(){
 
         for(int i = 0; i < gif.size();i++){
-            System.out.println("CLEAR");
+            this.clearConsole();
+
             System.out.println(gif.get(i));
             try {
                 if(indexRepare.contains(i))
@@ -140,6 +153,32 @@ public class AsciiBath {
 
         }
 
+    }
+
+    private void clearConsole(){
+        String modeRun = "dev";
+        if(modeRun == "dev"){
+            /* !! ATTENTION !! Ne fonctionne que si GREP CONSOLE est installé
+                Dans les paramètres =====> Ajouter une ligne 'filtering', mettre en expression ' .*CLEAR.*  '
+                tout cocher, y compris 'clear console' et enregistrer
+            */
+            System.out.println("CLEAR");
+        }
+
+        if(modeRun == "prod"){
+            // Ne fonctionne pas dans la console de l'IDE
+            try{
+                String os = System.getProperty("os.name");
+                if (os.contains("Windows")){
+                    Runtime.getRuntime().exec("cls");
+                }else{
+                    Runtime.getRuntime().exec("clear");
+                }
+            }
+            catch (Exception e){
+                System.out.println("Oops");
+            }
+        }
     }
 
 }
